7a8a54cbae744676dba6242260051de8393fe8c8
RollyLif <relifungula@gmail.com>
2022-10-24 11:48:22 +0200

    indiquer au serveur qu'on utilise le moteur de template EJS

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Express = require('express');
 const app = Express();
 const PORT = 4000 ;
 
+app.set("view engine", "ejs");
 
 app.listen(4000, () => {
     console.log("Serveur démarré au port : 4000!");
