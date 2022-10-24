3e49df88e7a4a465f17701d1dcfb0f18c87298b5
ItVal <valentinhonvj1@gmail.com>
2022-10-24 12:00:11 +0200

    Appel de la variable PORT au console

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.listen(4000, () => {
-    console.log("Serveur démarré au port : 4000!");
+    console.log("Serveur démarré au port : " + PORT);
   });
