98ef535008352c297aedf19d3a110809524cf7eb
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2022-10-24 12:15:22 +0200

    update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const Express = require('express');
+const Express = require("express");
 const path = require("path");
 const app = Express();
-const PORT = 4000 ;
+const PORT = 4000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.listen(4000, () => {
-    console.log("Serveur démarré au port : 4000!");
-  });
+  console.log("Serveur démarré au port : 4000!");
+});
