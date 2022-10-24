4eafd3752524e687c5331ec42ccde01fb66a93c2
RollyLif <relifungula@gmail.com>
2022-10-24 12:03:02 +0200

    definition de la route par défaut

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const PORT = 4000 ;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.get("/", (req, res) => {
+  res.send("Bonjour le monde...");
+  res.render("index");
+});
+
 app.listen(4000, () => {
     console.log("Serveur démarré au port : 4000!");
   });
