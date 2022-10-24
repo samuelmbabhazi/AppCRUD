59ec047bcdad9c6e491e479be5102f8214b3bca5
Doddy MATABARO <matabarododdy@gmail.com>
2022-10-24 11:55:10 +0200

    ajout a la connexion a la base de donnees

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const Express = require('express');
 const app = Express();
+const { Pool } = require("pg");
 const PORT = 4000 ;
 
+const pool = new Pool({
+  user: "dsiqfcrw ",
+  host: "peanut.db.elephantsql.com",
+  database: "dsiqfcrw",
+  password: "wmJq60NzgDASt9hYYt1FI2JAPkUWepV9",
+  port: 5432
+});
+
+console.log("Connexion réussie à la base de données");
 
 app.listen(4000, () => {
     console.log("Serveur démarré au port : 4000!");
